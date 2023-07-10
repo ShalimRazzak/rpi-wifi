23ba924bab7315ac8ce964d36dcc9d15f828d843
Shalim Razzak <132011667+ShalimRazzak@users.noreply.github.com>
2023-07-09 21:41:14 -0500

    Update bullseyePIxT.sh

--- bullseyePIxT.sh
+++ bullseyePIxT.sh
@@ -238,6 +238,7 @@ fi
 
 if test true != "${STA_ONLY}"; then
     # Populate `/etc/hostapd/hostapd.conf`
+    sudo mkdir -p /etc/hostapd/hostapd.conf
     _logger "Populate /etc/hostapd/hostapd.conf"
     sudo bash -c 'cat > /etc/hostapd/hostapd.conf' <<EOF
 ctrl_interface=/var/run/hostapd
